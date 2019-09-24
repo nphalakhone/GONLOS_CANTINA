b64e525ab0d387f583bf98448cbf9b0b95286abe
lukeWoodley <49654317+lukeWoodley@users.noreply.github.com>
2019-09-24 17:45:35 -0400

    ajout round Panel pazaak
    .

--- Projet_GONLO/Projet_GONLO/Pazaak.Designer.cs
+++ Projet_GONLO/Projet_GONLO/Pazaak.Designer.cs
@@ -28,7 +28,205 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.RPnlG1 = new Projet_GONLO.RoundPanel();
+            this.RPnlG2 = new Projet_GONLO.RoundPanel();
+            this.RPnlG3 = new Projet_GONLO.RoundPanel();
+            this.RPnlG4 = new Projet_GONLO.RoundPanel();
+            this.RPnlG5 = new Projet_GONLO.RoundPanel();
+            this.RPnlG6 = new Projet_GONLO.RoundPanel();
+            this.RPnlG7 = new Projet_GONLO.RoundPanel();
+            this.RPnlG8 = new Projet_GONLO.RoundPanel();
+            this.RPnlG9 = new Projet_GONLO.RoundPanel();
+            this.RPnlD1 = new Projet_GONLO.RoundPanel();
+            this.RPnlD2 = new Projet_GONLO.RoundPanel();
+            this.RPnlD3 = new Projet_GONLO.RoundPanel();
+            this.RPnlD4 = new Projet_GONLO.RoundPanel();
+            this.RPnlD5 = new Projet_GONLO.RoundPanel();
+            this.RPnlD6 = new Projet_GONLO.RoundPanel();
+            this.RPnlD7 = new Projet_GONLO.RoundPanel();
+            this.RPnlD8 = new Projet_GONLO.RoundPanel();
+            this.RPnlD9 = new Projet_GONLO.RoundPanel();
             this.SuspendLayout();
+            // 
+            // RPnlG1
+            // 
+            this.RPnlG1.BackColor = System.Drawing.Color.White;
+            this.RPnlG1.BorderColor = System.Drawing.Color.White;
+            this.RPnlG1.Edge = 20;
+            this.RPnlG1.Location = new System.Drawing.Point(200, 135);
+            this.RPnlG1.Name = "RPnlG1";
+            this.RPnlG1.Size = new System.Drawing.Size(73, 89);
+            this.RPnlG1.TabIndex = 0;
+            // 
+            // RPnlG2
+            // 
+            this.RPnlG2.BackColor = System.Drawing.Color.White;
+            this.RPnlG2.BorderColor = System.Drawing.Color.White;
+            this.RPnlG2.Edge = 20;
+            this.RPnlG2.Location = new System.Drawing.Point(294, 135);
+            this.RPnlG2.Name = "RPnlG2";
+            this.RPnlG2.Size = new System.Drawing.Size(73, 89);
+            this.RPnlG2.TabIndex = 1;
+            // 
+            // RPnlG3
+            // 
+            this.RPnlG3.BackColor = System.Drawing.Color.White;
+            this.RPnlG3.BorderColor = System.Drawing.Color.White;
+            this.RPnlG3.Edge = 20;
+            this.RPnlG3.Location = new System.Drawing.Point(386, 135);
+            this.RPnlG3.Name = "RPnlG3";
+            this.RPnlG3.Size = new System.Drawing.Size(73, 89);
+            this.RPnlG3.TabIndex = 2;
+            // 
+            // RPnlG4
+            // 
+            this.RPnlG4.BackColor = System.Drawing.Color.White;
+            this.RPnlG4.BorderColor = System.Drawing.Color.White;
+            this.RPnlG4.Edge = 20;
+            this.RPnlG4.Location = new System.Drawing.Point(200, 239);
+            this.RPnlG4.Name = "RPnlG4";
+            this.RPnlG4.Size = new System.Drawing.Size(73, 89);
+            this.RPnlG4.TabIndex = 3;
+            // 
+            // RPnlG5
+            // 
+            this.RPnlG5.BackColor = System.Drawing.Color.White;
+            this.RPnlG5.BorderColor = System.Drawing.Color.White;
+            this.RPnlG5.Edge = 20;
+            this.RPnlG5.Location = new System.Drawing.Point(294, 239);
+            this.RPnlG5.Name = "RPnlG5";
+            this.RPnlG5.Size = new System.Drawing.Size(73, 89);
+            this.RPnlG5.TabIndex = 4;
+            // 
+            // RPnlG6
+            // 
+            this.RPnlG6.BackColor = System.Drawing.Color.White;
+            this.RPnlG6.BorderColor = System.Drawing.Color.White;
+            this.RPnlG6.Edge = 20;
+            this.RPnlG6.Location = new System.Drawing.Point(386, 239);
+            this.RPnlG6.Name = "RPnlG6";
+            this.RPnlG6.Size = new System.Drawing.Size(73, 89);
+            this.RPnlG6.TabIndex = 5;
+            // 
+            // RPnlG7
+            // 
+            this.RPnlG7.BackColor = System.Drawing.Color.White;
+            this.RPnlG7.BorderColor = System.Drawing.Color.White;
+            this.RPnlG7.Edge = 20;
+            this.RPnlG7.Location = new System.Drawing.Point(200, 344);
+            this.RPnlG7.Name = "RPnlG7";
+            this.RPnlG7.Size = new System.Drawing.Size(73, 89);
+            this.RPnlG7.TabIndex = 6;
+            // 
+            // RPnlG8
+            // 
+            this.RPnlG8.BackColor = System.Drawing.Color.White;
+            this.RPnlG8.BorderColor = System.Drawing.Color.White;
+            this.RPnlG8.Edge = 20;
+            this.RPnlG8.Location = new System.Drawing.Point(294, 344);
+            this.RPnlG8.Name = "RPnlG8";
+            this.RPnlG8.Size = new System.Drawing.Size(73, 89);
+            this.RPnlG8.TabIndex = 7;
+            // 
+            // RPnlG9
+            // 
+            this.RPnlG9.BackColor = System.Drawing.Color.White;
+            this.RPnlG9.BorderColor = System.Drawing.Color.White;
+            this.RPnlG9.Edge = 20;
+            this.RPnlG9.Location = new System.Drawing.Point(386, 344);
+            this.RPnlG9.Name = "RPnlG9";
+            this.RPnlG9.Size = new System.Drawing.Size(73, 89);
+            this.RPnlG9.TabIndex = 8;
+            // 
+            // RPnlD1
+            // 
+            this.RPnlD1.BackColor = System.Drawing.Color.White;
+            this.RPnlD1.BorderColor = System.Drawing.Color.White;
+            this.RPnlD1.Edge = 20;
+            this.RPnlD1.Location = new System.Drawing.Point(542, 135);
+            this.RPnlD1.Name = "RPnlD1";
+            this.RPnlD1.Size = new System.Drawing.Size(73, 89);
+            this.RPnlD1.TabIndex = 9;
+            // 
+            // RPnlD2
+            // 
+            this.RPnlD2.BackColor = System.Drawing.Color.White;
+            this.RPnlD2.BorderColor = System.Drawing.Color.White;
+            this.RPnlD2.Edge = 20;
+            this.RPnlD2.Location = new System.Drawing.Point(640, 135);
+            this.RPnlD2.Name = "RPnlD2";
+            this.RPnlD2.Size = new System.Drawing.Size(73, 89);
+            this.RPnlD2.TabIndex = 10;
+            // 
+            // RPnlD3
+            // 
+            this.RPnlD3.BackColor = System.Drawing.Color.White;
+            this.RPnlD3.BorderColor = System.Drawing.Color.White;
+            this.RPnlD3.Edge = 20;
+            this.RPnlD3.Location = new System.Drawing.Point(729, 135);
+            this.RPnlD3.Name = "RPnlD3";
+            this.RPnlD3.Size = new System.Drawing.Size(73, 89);
+            this.RPnlD3.TabIndex = 11;
+            // 
+            // RPnlD4
+            // 
+            this.RPnlD4.BackColor = System.Drawing.Color.White;
+            this.RPnlD4.BorderColor = System.Drawing.Color.White;
+            this.RPnlD4.Edge = 20;
+            this.RPnlD4.Location = new System.Drawing.Point(542, 239);
+            this.RPnlD4.Name = "RPnlD4";
+            this.RPnlD4.Size = new System.Drawing.Size(73, 89);
+            this.RPnlD4.TabIndex = 12;
+            // 
+            // RPnlD5
+            // 
+            this.RPnlD5.BackColor = System.Drawing.Color.White;
+            this.RPnlD5.BorderColor = System.Drawing.Color.White;
+            this.RPnlD5.Edge = 20;
+            this.RPnlD5.Location = new System.Drawing.Point(640, 239);
+            this.RPnlD5.Name = "RPnlD5";
+            this.RPnlD5.Size = new System.Drawing.Size(73, 89);
+            this.RPnlD5.TabIndex = 13;
+            // 
+            // RPnlD6
+            // 
+            this.RPnlD6.BackColor = System.Drawing.Color.White;
+            this.RPnlD6.BorderColor = System.Drawing.Color.White;
+            this.RPnlD6.Edge = 20;
+            this.RPnlD6.Location = new System.Drawing.Point(729, 239);
+            this.RPnlD6.Name = "RPnlD6";
+            this.RPnlD6.Size = new System.Drawing.Size(73, 89);
+            this.RPnlD6.TabIndex = 14;
+            // 
+            // RPnlD7
+            // 
+            this.RPnlD7.BackColor = System.Drawing.Color.White;
+            this.RPnlD7.BorderColor = System.Drawing.Color.White;
+            this.RPnlD7.Edge = 20;
+            this.RPnlD7.Location = new System.Drawing.Point(542, 344);
+            this.RPnlD7.Name = "RPnlD7";
+            this.RPnlD7.Size = new System.Drawing.Size(73, 89);
+            this.RPnlD7.TabIndex = 15;
+            // 
+            // RPnlD8
+            // 
+            this.RPnlD8.BackColor = System.Drawing.Color.White;
+            this.RPnlD8.BorderColor = System.Drawing.Color.White;
+            this.RPnlD8.Edge = 20;
+            this.RPnlD8.Location = new System.Drawing.Point(640, 344);
+            this.RPnlD8.Name = "RPnlD8";
+            this.RPnlD8.Size = new System.Drawing.Size(73, 89);
+            this.RPnlD8.TabIndex = 16;
+            // 
+            // RPnlD9
+            // 
+            this.RPnlD9.BackColor = System.Drawing.Color.White;
+            this.RPnlD9.BorderColor = System.Drawing.Color.White;
+            this.RPnlD9.Edge = 20;
+            this.RPnlD9.Location = new System.Drawing.Point(729, 344);
+            this.RPnlD9.Name = "RPnlD9";
+            this.RPnlD9.Size = new System.Drawing.Size(73, 89);
+            this.RPnlD9.TabIndex = 17;
             // 
             // Pazaak
             // 
@@ -37,6 +235,24 @@
             this.BackgroundImage = global::Projet_GONLO.Properties.Resources.pazaakEmptyBoard11;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(1001, 750);
+            this.Controls.Add(this.RPnlD9);
+            this.Controls.Add(this.RPnlD8);
+            this.Controls.Add(this.RPnlD7);
+            this.Controls.Add(this.RPnlD6);
+            this.Controls.Add(this.RPnlD5);
+            this.Controls.Add(this.RPnlD4);
+            this.Controls.Add(this.RPnlD3);
+            this.Controls.Add(this.RPnlD2);
+            this.Controls.Add(this.RPnlD1);
+            this.Controls.Add(this.RPnlG9);
+            this.Controls.Add(this.RPnlG8);
+            this.Controls.Add(this.RPnlG7);
+            this.Controls.Add(this.RPnlG6);
+            this.Controls.Add(this.RPnlG5);
+            this.Controls.Add(this.RPnlG4);
+            this.Controls.Add(this.RPnlG3);
+            this.Controls.Add(this.RPnlG2);
+            this.Controls.Add(this.RPnlG1);
             this.DoubleBuffered = true;
             this.MaximizeBox = false;
             this.Name = "Pazaak";
@@ -46,5 +262,24 @@
         }
 
         #endregion
+
+        private RoundPanel RPnlG1;
+        private RoundPanel RPnlG2;
+        private RoundPanel RPnlG3;
+        private RoundPanel RPnlG4;
+        private RoundPanel RPnlG5;
+        private RoundPanel RPnlG6;
+        private RoundPanel RPnlG7;
+        private RoundPanel RPnlG8;
+        private RoundPanel RPnlG9;
+        private RoundPanel RPnlD1;
+        private RoundPanel RPnlD2;
+        private RoundPanel RPnlD3;
+        private RoundPanel RPnlD4;
+        private RoundPanel RPnlD5;
+        private RoundPanel RPnlD6;
+        private RoundPanel RPnlD7;
+        private RoundPanel RPnlD8;
+        private RoundPanel RPnlD9;
     }
 }
