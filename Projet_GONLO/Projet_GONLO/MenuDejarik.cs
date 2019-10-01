4310d4d1c311112a7b5f92b99b885e2cf312c24f
Gdelb <gui.dlb.37@gmail.com>
2019-09-30 21:00:05 -0400

    Menu Dejarik

---
+++ Projet_GONLO/Projet_GONLO/MenuDejarik.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projet_GONLO
+{
+    public partial class MenuDejarik : Form
+    {
+        public MenuDejarik()
+        {
+            InitializeComponent();
+            PnlChooseMonster.Hide();
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
