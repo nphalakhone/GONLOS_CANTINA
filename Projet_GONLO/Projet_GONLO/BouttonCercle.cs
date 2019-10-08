5dfc3e68aeb82df509f75eac039a4a28031246c6
nphalakhone <nick.phalakhone@gmail.com>
2019-10-08 10:09:23 -0400

    Creation Bouttons Dejarik

--- Projet_GONLO/Projet_GONLO/BouttonCercle.cs
+++ Projet_GONLO/Projet_GONLO/BouttonCercle.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace Projet_GONLO
 {
-    class BouttonCercle
+    public class BouttonCercle : Button
     {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(gp);
+            base.OnPaint(e);
+        }
     }
 }
--- Projet_GONLO/Projet_GONLO/Program.cs
+++ Projet_GONLO/Projet_GONLO/Program.cs
@@ -16,9 +16,9 @@ namespace Projet_GONLO
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new MenuAccueil());
+            Application.Run(new MenuAccueil());
             //Application.Run(new CreationPerso());
-            Application.Run(new MenuDejarik());
+            //Application.Run(new MenuDejarik());
 
 
 
