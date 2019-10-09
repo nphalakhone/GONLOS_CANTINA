3ddc56f2558722f1abf9d60cf175a6680b6a9080
RSP006 <o.cadieux006@hotmail.ca>
2019-10-09 15:04:00 -0400

    Pazaak card selector

---
+++ Projet_GONLO/Projet_GONLO/PazaakCardsSelector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projet_GONLO
+{
+    public partial class PazaakCardsSelector : Form
+    {
+        public PazaakCardsSelector()
+        {
+            InitializeComponent();
+        }
+
+        private void RPnl1Plus_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
