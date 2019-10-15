5e5b71197a98131b9e309cd5dee3aec4a383b7e7
Gdelb <gui.dlb.37@gmail.com>
2019-10-15 09:03:17 -0400

    implement computer pick

--- Projet_GONLO/Projet_GONLO/Forms/Dejarik/MenuDejarik.cs
+++ Projet_GONLO/Projet_GONLO/Forms/Dejarik/MenuDejarik.cs
@@ -136,20 +136,53 @@ namespace Projet_GONLO
             }
             else
             {
-
+                computerPick();
+                checkMonsterFinish();
             }
+        }
+
+        private void computerPick()
+        {
+            if (BtnGhhhk.Enabled == true && BtnHoujix.Enabled == true)
+            {
+                String message = "The computer chose the monster Ghhhk, you have then be assigned the monster Houjix";
+                computerChoose(BtnGhhhk, BtnHoujix, message);
+            }
+            else if (BtnKintanStrider.Enabled == true && BtnNgok.Enabled == true)
+            {
+                String message = "The computer chose the monster Kintan Strider, you have then be assigned the monster Ngok";
+                computerChoose(BtnKintanStrider, BtnNgok, message);
+            }
+            else if (BtnKlorslug.Enabled == true && BtnGrimtaash.Enabled == true)
+            {
+                String message = "The computer chose the monster Klorslug, you have then be assigned the monster Grimstaash";
+                computerChoose(BtnKlorslug, BtnGrimtaash, message);
+            }
+            else
+            {
+                String message = "The computer chose the monster Mantellian, you have then be assigned the monster Monnok";
+                computerChoose(BtnMantellian, BtnMonnok, message);
+            }
+
+        }
+
+        private void computerChoose(Button btnGhhhk, Button btnHoujix, string message)
+        {
+            btnGhhhk.Enabled = false;
+            btnHoujix.Enabled = false;
+            MessageBox.Show(message);
         }
 
         private void changeLabel()
         {
-            if (LblMenuPlayerTurn.Text.Equals("Player 1's Turn to Pick"))
+            if (LblMenuPlayerTurn.Text.Equals("Player 1 choose one monster for each category  "))
             {
-                LblMenuPlayerTurn.Text = "Player 2's Turn to Pick";
+                LblMenuPlayerTurn.Text = "Player 2 choose one monster for each category  ";
                 Refresh();
             }
             else
             {
-                LblMenuPlayerTurn.Text = "Player 1's Turn to Pick";
+                LblMenuPlayerTurn.Text = "Player 1 choose one monster for each category  ";
                 Refresh();
             }
         }
