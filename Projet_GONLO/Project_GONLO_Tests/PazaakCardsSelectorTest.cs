84c6085c0749ce70cd995d49555d33eb4e80e4f8
lukeWoodley <49654317+lukeWoodley@users.noreply.github.com>
2019-11-25 20:23:53 -0500

    Update PazaakCardsSelectorTest.cs

--- Projet_GONLO/Project_GONLO_Tests/PazaakCardsSelectorTest.cs
+++ Projet_GONLO/Project_GONLO_Tests/PazaakCardsSelectorTest.cs
@@ -5,14 +5,15 @@ namespace Project_GONLO_Tests
     [TestClass]
     public class PazaakCardsSelectorTest
     {
-
+        Player player;
 
 
         [TestMethod]
         public void TestMethod1()
         {
-            
-            Player player = new Player();
+
+            //Arrange
+            player = new Player();
             player.Name = "Test_Name";
             player.Credits = 500;
             player.PazaakGamesWon = 0;
@@ -20,7 +21,14 @@ namespace Project_GONLO_Tests
             player.DejarikGamesLost = 0;
             player.DejarikGamesWon = 0;
             PazaakCardsSelector pazaakCards = new PazaakCardsSelector();
-            pazaakCards.= player;
+            pazaakCards.Player1 = player;
+
+            //Act
+            string nameSentForm = pazaakCards.Player1.Name;
+            string name = player.Name;
+
+            //Assert
+            Assert.AreEqual(name, nameSentForm);
         }
     }
 }
