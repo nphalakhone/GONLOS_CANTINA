62a92f01ef405bae203213f79a8bd9679a7fd9e6
Lourmati <49345524+Lourmati@users.noreply.github.com>
2019-12-01 15:42:07 -0500

    TEST INITIALIZE BUTTON

--- Projet_GONLO/Projet_GONLO_Tests/DejarikTests.cs
+++ Projet_GONLO/Projet_GONLO_Tests/DejarikTests.cs
@@ -1,14 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projet_GONLO;
+using Projet_GONLO.Classes;
 
 namespace Projet_GONLO_Tests
 {
     [TestClass]
     public class DejarikTest
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void InitializeListButtonsTest()
         {
+            //dejarikTest = new PrivateObject(typeof(Dejarik), "");
+            //dejarikTest.Invoke("initalizeListButtons");//call method
+            //List<Button> listButtons = (List<Button>)dejarikTest.GetField("listButtons");//get the list
+            //Assert.AreEqual(26, listButtons.Count);
+
+            var DejarikTest = new Dejarik("");
+            var privatedejarikTest = new PrivateObject(DejarikTest);
+            privatedejarikTest.Invoke("initalizeListButtons");
+            List<Button> listButtons = (List<Button>)privatedejarikTest.GetField("listButtons");
+            Assert.AreEqual(26, listButtons.Count);
         }
     }
 }
